010726a636093f0354bd4b4a1506ae4cbef1fc98
Josh Zylstra <zylstrajosh@gmail.com>
2016-03-24 11:12:47 -0700

    herewego

---
+++ app/models/user.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('User', new Schema({
+  name: String,
+  password: String,
+  admin: Boolean
+}));
